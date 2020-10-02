b56d2d75c745a1dc0718f7f549e4ad3fc12085a6
Heather Hern <heathernewlin1@gmail.com>
2020-10-02 16:50:40 -0400

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,30 @@ const fs = require("fs");
 // array of questions for user
 const questions = [
 
+    {
+        type: "input",
+        message: "GitHub Username",
+        name: "username"
+    },
+
+    {
+        type: "input",
+        message: "README Title",
+        name: "username"
+    },
+
+    {
+        type: "input",
+        message: "Project Description",
+        name: "username"
+    },
+
+    {
+        type: "input",
+        message: "Project Authors",
+        name: "username"
+    },
+
 ];
 
 // function to write README file
@@ -18,3 +42,6 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
