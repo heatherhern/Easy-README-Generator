342d21140af6a3cddeea7b68c67f51bd3e37d2e5
Heather Hern <heathernewlin1@gmail.com>
2020-10-07 19:25:25 -0400

    code cleanup

--- Develop/index.js
+++ Develop/index.js
@@ -165,16 +165,8 @@ module.exports = { generateMarkdown };
 
 
 // // function to write README file
-// function writeToFile(fileName, data) {
-// }
+
 
 // // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
-
 
 // // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
